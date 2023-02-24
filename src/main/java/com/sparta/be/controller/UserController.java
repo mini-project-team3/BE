285119f251765a9eec0e57bb55ecf3621419dda4
Ganpyeon <jms03096@naver.com>
2023-02-24 22:52:26 +0900

    feat:#9 로그인 API 구현

--- src/main/java/com/sparta/be/controller/UserController.java
+++ src/main/java/com/sparta/be/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.sparta.be.controller;
 
 import com.sparta.be.common.ApiResponseDto;
+import com.sparta.be.dto.LoginRequestDto;
 import com.sparta.be.dto.SignupRequestDto;
 import com.sparta.be.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,5 +21,10 @@ public class UserController {
     @PostMapping("/signup")
     public ApiResponseDto<?> signup(@RequestBody SignupRequestDto signupRequestDto){
         return userService.signup(signupRequestDto);
+    }
+
+    @PostMapping("/login")
+    public ApiResponseDto<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
+        return userService.login(loginRequestDto, response);
     }
 }
--- src/main/java/com/sparta/be/service/UserService.java
+++ src/main/java/com/sparta/be/service/UserService.java
@@ -3,9 +3,12 @@ package com.sparta.be.service;
 import com.sparta.be.common.ApiResponseDto;
 import com.sparta.be.common.ErrorResponse;
 import com.sparta.be.common.ResponseUtils;
+import com.sparta.be.dto.LoginRequestDto;
 import com.sparta.be.dto.SignupRequestDto;
 import com.sparta.be.entity.User;
+import com.sparta.be.jwt.JwtUtil;
 import com.sparta.be.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,8 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
@@ -35,6 +40,27 @@ public class UserService {
 
         User user = new User(loginId, password, nickName);
         userRepository.save(user);
+        return ResponseUtils.ok();
+
+    }
+
+    @Transactional
+    public ApiResponseDto<?> login(LoginRequestDto loginRequestDto, HttpServletResponse response) { // 회원 가입
+        String loginId = loginRequestDto.getLoginId();
+        String password = loginRequestDto.getPassword();
+
+        //사용자 확인
+        if (userRepository.findByLoginId(loginId).isEmpty()) {
+            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST, "등록된 사용자가 없습니다."));
+        }
+
+        //비밀번호 중복 확인
+        User user = userRepository.findByLoginId(loginId).get();
+        if (!passwordEncoder.matches(password,loginRequestDto.getPassword())){
+            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."));
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getLoginId()));
         return ResponseUtils.ok();
 
     }
