943dddc30b4740abd1e53f0684631fd853c406e2
Ganpyeon <jms03096@naver.com>
2023-02-26 02:15:07 +0900

    feat: #29 좋아요 Entity, DTO 설계 및 추가 구현

--- src/main/java/com/sparta/be/dto/ReviewResponseDto.java
+++ src/main/java/com/sparta/be/dto/ReviewResponseDto.java
@@ -19,5 +19,6 @@ public class ReviewResponseDto {
         this.title = review.getTitle();
         this.nickname = review.getUser().getNickname();
         this.createdAt = review.getCreatedAt();
+        this.likeCount = review.getLikeReview() != null ? review.getLikeReview().size() : 0; // 리스트에 저장된 목록이 null이 아닐때 리스트에 저장된 좋아요 갯수를 반환 하고 맞으면 0으로 반환
     }
 }
