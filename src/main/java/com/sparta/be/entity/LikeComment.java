275e1790840e2610be42fedeb3019aa0d5107201
Ganpyeon <jms03096@naver.com>
2023-02-26 02:17:45 +0900

    feat: #29 좋아요 Entity, DTO 설계 및 추가 구현

---
+++ src/main/java/com/sparta/be/entity/LikeComment.java
@@ -0,0 +1,22 @@
+package com.sparta.be.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
