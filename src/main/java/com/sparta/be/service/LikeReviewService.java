67d81c0efae77e2d2b60ffd863e8ec7ca7ce20b5
Ganpyeon <jms03096@naver.com>
2023-02-28 13:42:47 +0900

    feat : #44 LikeReview 관련 excetpion 처리 적용

--- src/main/java/com/sparta/be/service/LikeReviewService.java
+++ src/main/java/com/sparta/be/service/LikeReviewService.java
@@ -1,7 +1,7 @@
 package com.sparta.be.service;
 
 import com.sparta.be.common.ApiResponseDto;
-import com.sparta.be.common.ErrorResponse;
+import com.sparta.be.common.ErrorType;
 import com.sparta.be.common.ResponseUtils;
 import com.sparta.be.entity.LikeReview;
 import com.sparta.be.entity.Review;
@@ -9,7 +9,6 @@ import com.sparta.be.entity.User;
 import com.sparta.be.repository.LikeReviewRepository;
 import com.sparta.be.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,11 +22,11 @@ public class LikeReviewService {
     private final ReviewRepository reviewRepository;
 
     @Transactional
-    public ApiResponseDto<?> likeReview(Long id, User user){
+    public ApiResponseDto<Void> likeReview(Long id, User user){
         //리뷰 확인
         Optional<Review> review = reviewRepository.findById(id);
         if (review.isEmpty()){
-            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST,"리뷰가 존재하지 않습니다."));
+            throw new IllegalArgumentException(ErrorType.NOT_FOUND_REVIEW_WRITING.getMessage());
         }
 
 
@@ -37,18 +36,18 @@ public class LikeReviewService {
             LikeReview likeReview = LikeReview.of(review.get(), user);
             likeReviewRepository.save(likeReview);
         }else {
-            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST, "좋아요가 이미 되어있습니다."));
+           throw new IllegalArgumentException(ErrorType.LIKE_ALREADY_DONE.getMessage());
         }
 
             return ResponseUtils.ok();
     }
 
     @Transactional
-    public ApiResponseDto<?> likeCancelReview(Long id, User user){
+    public ApiResponseDto<Void> likeCancelReview(Long id, User user){
         //리뷰 확인
         Optional<Review> review = reviewRepository.findById(id);
         if (review.isEmpty()){
-            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST,"리뷰가 존재하지 않습니다."));
+            throw new IllegalArgumentException(ErrorType.NOT_FOUND_REVIEW_WRITING.getMessage());
         }
 
 
@@ -58,7 +57,7 @@ public class LikeReviewService {
             likeReviewRepository.delete(found.get());
             likeReviewRepository.flush();
         }else {
-            return ResponseUtils.error(ErrorResponse.of(HttpStatus.BAD_REQUEST, "좋아요가 이미 취소 되어있습니다."));
+            throw new IllegalArgumentException(ErrorType.LIKE_CANCLE_ALREADY_DONE.getMessage());
         }
 
         return ResponseUtils.ok();
